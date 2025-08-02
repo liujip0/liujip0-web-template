b25435977e4639197315e1842d6250531508fc7c
liujip0 <liujip2020@gmail.com>
2025-08-01 20:51:41 -0400

    set up routes on api

---
+++ api/src/context.ts
@@ -0,0 +1,18 @@
+import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
+import { Env } from "./index.ts";
+
+export const createContext = async ({
+  req,
+  env,
+  resHeaders,
+}: FetchCreateContextFnOptions & {
+  env: Env;
+}) => {
+  return {
+    req,
+    env,
+    resHeaders,
+  };
+};
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
---
+++ api/src/index.ts
@@ -0,0 +1,26 @@
+import { D1Database } from "@cloudflare/workers-types";
+import {
+  FetchCreateContextFnOptions,
+  fetchRequestHandler,
+} from "@trpc/server/adapters/fetch";
+import { createContext } from "./context.ts";
+import { appRouter } from "./routes/routes.ts";
+
+export interface Env {
+  DB: D1Database;
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return fetchRequestHandler({
+      endpoint: "/api",
+      req: request,
+      router: appRouter,
+      createContext: (options: FetchCreateContextFnOptions) =>
+        createContext({
+          ...options,
+          env,
+        }),
+    });
+  },
+};
